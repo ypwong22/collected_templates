2d275e29327d73ff6d9612a08e7ffe2c1d6aa09f
ypwong22 <ypwong22@users.noreply.github.com>
2019-12-05 19:58:28 -0500

    Update regional_summary.py

--- regional_summary.py
+++ regional_summary.py
@@ -22,10 +22,12 @@ var = xr.DataArray(np.where(mask, data['var'].values.copy(), np.nan),
 data.close()
 
 # Cut down the dataset to only a bounding box of the mask.
+eps = 0.01
+
 mask_lat = data.lat.values[np.isfinite(mask).any(axis = 1)]
 mask_lon = data.lon.values[np.isfinite(mask).any(axis = 0)]
-var = var.sel(lat = slice(np.min(mask_lat), np.max(mask_lat)),
-              lon = slice(np.min(mask_lon), np.max(mask_lon)))
+var = var.sel(lat = slice(np.min(mask_lat) - eps, np.max(mask_lat) + eps),
+              lon = slice(np.min(mask_lon) - eps, np.max(mask_lon) + eps))
 
 # Save to netcdf.
 var.to_dataset(name = 'var').to_netcdf('mydata_subset.nc')
@@ -44,23 +46,23 @@ plt.close(fig)
 import xarray as xr
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
 
 lat_bnd = np.arange(0., 60.1, 0.5)
+eps = 0.01 # Tolerance for floating point latitude comparison.
 
-fname = 'myfile.nc'
-data = xr.open_dataset(fname, decode_times = True)
-time = data['time'].indexes['time']
-var_ts = data['var'].mean(dim = ['lat', 'lon'])
+var_bylat = np.full(len(lat_bnd)-1, np.nan)
+
+data = xr.open_dataset('myfile.nc')
+for i in range(len(lat_bnd)-1):
+    var_bylat[i] = np.nanmean(data['var'].sel(lat = slice(lat_bnd[i] - eps, lat_bnd[i+1] - eps)))
 data.close()
 
-fig, ax = plt.subplots(figsize = (10, 10))
-ax.plot(time, var_ts, '-', color = 'k')
-ax.set_xlabel('Year')
+fig, ax = plt.subplots(figsize = (4, 4))
+ax.plot(0.5 * (lat_bnd[:-1] + lat_bnd[1:]), var_bylat, '-', color = 'k')
+ax.set_xlabel('Lat')
 ax.set_ylabel('Var [Unit]')
 fig.savefig('myfig.png', dpi = 600., bbox_inches = 'tight')
 plt.close(fig)
-
 
 ###############################################################################
 # Calculate and plot mean time series of individual regions defined by levels 
