4cbaedfa47b4f30c4b5a74e35e21be1358fbc441
ypwong22 <ypwong22@users.noreply.github.com>
2019-12-06 21:04:35 -0500

    Update analysis_2d.py

--- analysis_2d.py
+++ analysis_2d.py
@@ -1,11 +1,37 @@
 ###############################################################################
-# Remove trend from each column of a pandas dataframe
+# De-trend from each column of a pandas dataframe
 ###############################################################################
+import statsmodels.api as stats
 
+...
 
+data = ... # a pandas data frame
 
+data_detrend = data.copy(deep = True)
+for c in range(data.shape[1]):
+    x = range(data.shape[0])
+    mod = stats.OLS(data.iloc[:, c], stats.add_constant(x))
+    results = mod.fit()
+    
+    data_detrend.iloc[:, c] = data.iloc[:, c] - results.params[1] * x
 
 
 ###############################################################################
 # Fit and plot the trend function
 ###############################################################################
+import statsmodels.api as stats
+import matplotlib.pyplot as plt
+
+
+...
+
+time_series = [...]
+
+x = range(len(time_series))
+mod = stats.OLS(time_series, stats.add_constant(x))
+results = mod.fit()
+
+...
+
+ax.plot(x, results.params[0] + x * results.params[1], ...)
+ax.text(0.1, 0.1, ('%f (%.2f)' % (results.params[1], results.pvalues[1])))
