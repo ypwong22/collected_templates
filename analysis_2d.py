4543de90a91a080f587202e5cf50d71546edd138
ypwong22 <ypwong22@users.noreply.github.com>
2020-01-07 18:55:55 -0500

    Update analysis_2d.py

--- analysis_2d.py
+++ analysis_2d.py
@@ -39,7 +39,7 @@ ax.text(0.1, 0.1, ('%f (%.2f)' % (results.params[1], results.pvalues[1])))
 
 ###############################################################################
 # Calculate the annual + seasonal average of a time series. Assuming the
-# time series starts from Jan and ends in Dec.
+# time series starts from Jan and ends in Dec. Return DataFrame.
 ###############################################################################
 import pandas as pd
 import numpy as np
@@ -53,6 +53,36 @@ def seasonal_avg(pd_series):
 
     temp = pd_series.index.to_period(freq = 'Q-NOV')
     temp2 = pd_series.groupby(temp).mean()
+
+    # Assuming the time series starts from Jan and ends in Dec: 
+    # set the seasonal average of 2 months to NaN, and remove the
+    # last season (only contains a December)
+    temp2.iloc[0] = np.nan
+    temp2 = temp2.iloc[:-1]
+
+    for qtr, season in enumerate(['DJF', 'MAM', 'JJA', 'SON'], 1):
+        result[season] = temp2.loc[temp2.index.quarter == qtr]
+        result[season].index = result[season].index.year
+        
+    return pd.DataFrame(result)
+
+
+###############################################################################
+# Calculate the annual + seasonal average of a data frame. Assuming the
+# time series starts from Jan and ends in Dec. Return dictionary.
+###############################################################################
+import pandas as pd
+import numpy as np
+
+def seasonal_avg2(pd_frame):
+    """
+    Calculate the seasonal average of a pandas data frame.
+    """
+    result = {}
+    result['annual'] = pd_frame.groupby(pd_frame.index.year).mean()
+
+    temp = pd_frame.index.to_period(freq = 'Q-NOV')
+    temp2 = pd_frame.groupby(temp).mean()
 
     # Assuming the time series starts from Jan and ends in Dec: 
     # set the seasonal average of 2 months to NaN, and remove the
