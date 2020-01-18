85acb60ba476dc25d3d1c4e0c82b31e9f90b5c06
ypwong22 <ypwong22@users.noreply.github.com>
2020-01-18 13:58:27 -0500

    Update plot_2d.py

--- plot_2d.py
+++ plot_2d.py
@@ -1,17 +1,24 @@
 import matplotlib.pyplot as plt
 
-def plot_ts_shade(ax, time, ts = {'min': np.array([]), 'mean': np.array([]),
-                                  'max': np.array([])}, ts_label = '',
-                  ts_col = 'red'):
+def plot_ts_shade(ax, time, matrix, ts_label = '', ts_col = 'red',
+                  shade_col = 'red', alpha = 0.2, skipna = False):
     """
     Plot a shaded ensemble.
     """
-    hl, = ax.plot(time, ts['mean'], '-', color = ts_col, linewidth = 2,
+    if skipna:
+        ts_min = np.nanmin(matrix, axis = 1)
+        ts_mean = np.nanmeann(matrix, axis = 1)
+        ts_max = np.nanmax(matrix, axis = 1)
+    else:
+        ts_min = np.min(matrix, axis = 1)
+        ts_mean = np.meann(matrix, axis = 1)
+        ts_max = np.max(matrix, axis = 1)
+
+    hl, = ax.plot(time, ts_mean, '-', color = ts_col, linewidth = 2,
                   label = ts_label)
-    ax.plot(time, ts['min'], '--', color = ts_col, linewidth = 1)
-    ax.plot(time, ts['max'], '--', color = ts_col, linewidth = 1)
-    ax.fill_between(time, ts['min'], ts['max'],
-                    where = ts['max'] > ts['min'],
-                    facecolor = ts_col, alpha = 0.2)
-    hfill, = ax.fill(np.nan, np.nan, ts_col, alpha = 0.2)
+    ax.plot(time, ts_min, '--', color = shade_col, linewidth = 1)
+    ax.plot(time, ts_max, '--', color = shade_col, linewidth = 1)
+    ax.fill_between(time, ts_min, ts_max, where = ts_max > ts_min,
+                    facecolor = shade_col, alpha = alpha)
+    hfill, = ax.fill(np.nan, np.nan, shade_col, alpha = alpha)
     return hl, hfill
