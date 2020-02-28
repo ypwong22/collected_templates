b6de1bbbba9d1e81292c8d088543af6a7c064038
ypwong22 <ypwong22@users.noreply.github.com>
2020-02-28 16:32:32 -0500

    Update plot_2d.py

--- plot_2d.py
+++ plot_2d.py
@@ -1,6 +1,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+
+###############################################################################
+# Shaded time series.
+###############################################################################
 def plot_ts_shade(ax, time, matrix, ts_label = '', ts_col = 'red',
                   shade_col = 'red', alpha = 0.2, skipna = False):
     """
@@ -23,3 +27,24 @@ def plot_ts_shade(ax, time, matrix, ts_label = '', ts_col = 'red',
                     facecolor = shade_col, alpha = alpha)
     hfill, = ax.fill(np.nan, np.nan, facecolor = shade_col, alpha = alpha)
     return hl, hfill
+
+  
+###############################################################################
+# Colored table according to values
+###############################################################################
+
+# Obtain pandas data frame
+# df = ...
+
+fig, axes = plt.subplots(figsize=(8,13))
+ax.set_xticks([])
+ax.set_yticks([])
+ax.set_frame_on(False)
+
+norm = plt.Normalize(3, 6)
+colours = plt.cm.hot(norm(vals.values))
+the_table = ax.table(cellText=vals.values.astype(int),
+                     rowLabels=vals.index, colLabels=vals.columns,
+                     colWidths = [0.1]*vals.shape[1],
+                     loc='center', fontsize = 16,
+                     cellColours=colours)
