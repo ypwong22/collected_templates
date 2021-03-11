d5b49d0b3a9101d6f4ad7d4dc21379173e8a8a86
ypwong22 <ypwong22@users.noreply.github.com>
2021-03-11 00:02:28 -0500

    Update plot_table.py

--- plot_table.py
+++ plot_table.py
@@ -18,3 +18,39 @@ def panel_table(ax, df):
                          cellColours=colours)
 
     return the_table
+
+
+def panel_stack_bar(ax, x, matrix, color, edgecolor = None, **kwargs):
+    """Stacked barplot with separate positive & negative values."""
+    mat_pos = np.clip(matrix, 0., None)
+    mat_pos_cum = np.cumsum(mat_pos, axis = 0)
+    mat_neg = np.clip(matrix, None, 0.)
+    mat_neg_cum = np.cumsum(mat_neg, axis = 0)
+
+    if edgecolor == None:
+        edgecolor = color
+
+    h = [None] * len(color)
+    for i in range(matrix.shape[0]):
+        temp = mat_pos[i,:] # only plot positive values
+        filt = np.abs(temp) > 1e-6
+        if np.sum(filt) > 0:
+            if i == 0:
+                h[i] = ax.bar(x[filt], temp[filt], color = color[i],
+                              edgecolor = edgecolor[i], **kwargs)
+            else:
+                h[i] = ax.bar(x[filt], temp[filt],
+                              bottom = mat_pos_cum[i-1,:][filt],
+                              color = color[i], edgecolor = edgecolor[i],
+                              **kwargs)
+        temp = mat_neg[i,:]
+        filt = np.abs(temp) > 1e-6
+        if np.sum(filt) > 0:
+            if i == 0:
+                ax.bar(x[filt], temp[filt], color = color[i],
+                       edgecolor = edgecolor[i], **kwargs)
+            else:
+                ax.bar(x[filt], temp[filt],
+                       bottom = mat_neg_cum[i-1,:][filt],
+                       color = color[i], edgecolor = edgecolor[i], **kwargs)
+    return h
