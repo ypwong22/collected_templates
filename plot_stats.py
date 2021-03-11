34ff9dc050000af6bb7cfe38078ac7699e46d609
ypwong22 <ypwong22@users.noreply.github.com>
2021-03-11 00:00:51 -0500

    Update plot_stats.py

--- plot_stats.py
+++ plot_stats.py
@@ -1,22 +1,30 @@
 import numpy as np
 from scipy.stats import norm
 from statsmodels.tsa.stattools import acf
+from sklearn.neighbors import KernelDensity
 
-
-def panel_histogram(ax, vector, bins,
+def panel_histogram(ax, vector, bins, dist = 'norm',
                     args_hist = {'color': '#bdbdbd', 'edgecolor': '#636363'},
                     args_line = {'color': '#2b8cbe'}):
     """
     Plot a histogram in panel with fitted Gaussian distribution.
+    dist: *norm* or *kde*
     """
     vector = vector[~np.isnan(vector)]
     hist, bin_edges = np.histogram(vector, bins = bins, density = True)
-    mean, std = norm.fit(vector)
-    x = np.linspace(bin_edges[0], bin_edges[-1], 100)
-
     ax.bar(bin_edges[:-1], hist, width = bin_edges[1:] - bin_edges[:-1],
            align = 'edge', **args_hist)
-    ax.plot(x, norm.pdf(x, mean, std), **args_line)
+    
+    x = np.linspace(bin_edges[0], bin_edges[-1], 100)
+
+    if dist == 'norm':
+        mean, std = norm.fit(vector)
+        ax.plot(x, norm.pdf(x, mean, std), **args_line)
+    elif dist == 'kde':
+        kde = KernelDensity(bandwidth = np.std(vector)/5, kernel='gaussian')
+        kde.fit(vector)
+        prob = np.exp(kde.score_samples(x))
+        ax.plot(x, prob, '-', **args_line)
 
 
 def panel_acf(ax, vector, max_lag = 12,
