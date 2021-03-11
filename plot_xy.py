9890316d8a00d0d141f1ed59853a04e00cf2815b
ypwong22 <ypwong22@users.noreply.github.com>
2021-03-10 23:56:16 -0500

    Update plot_xy.py

--- plot_xy.py
+++ plot_xy.py
@@ -1,6 +1,7 @@
 from statsmodels.tsa.stattools import acf
 from statsmodels.regression.linear_model import OLS
 from statsmodels.tools.tools import add_constant
+from scipy.stats import gaussian_kde
 
 
 def panel_trend(ax, vector, pos_xy = [0.1, 0.9],
@@ -24,3 +25,30 @@ def panel_trend(ax, vector, pos_xy = [0.1, 0.9],
             ppf(res.params[1], res.params[0],
                 res.pvalues[1], res.pvalues[0]),
             transform = ax.transAxes, **args_tx)
+
+
+def plot_scatter_density(ax, x, y, cmap = 'jet'):
+    """Plot 2D scatter plot, colored by local density."""
+    loc = np.vstack([x.ravel(), y.ravel()])
+    f = gaussian_kde(loc)
+    density = f(loc)
+    h = ax.scatter(x, y, c = density, cmap = cmap,
+                   edgecolor = '')
+    return h
+  
+
+def plot_colored_density(ax, x, y, cmap = 'jet'):
+    """Plot 2D Gaussian kde density plot."""
+    loc = np.vstack([x.ravel(), y.ravel()])
+    f = gaussian_kde(loc)
+
+    x_range = np.linspace(np.min(x) - (np.max(x) - np.min(x))/10,
+                          np.max(x) + (np.max(x) - np.min(x))/10, 100)
+    y_range = np.linspace(np.min(y) - (np.max(y) - np.min(y))/10,
+                          np.max(y) + (np.max(y) - np.min(y))/10, 100)
+    x_mesh, y_mesh = np.meshgrid(x_range, y_range)
+    z_mesh = f(np.vstack([x_mesh.ravel(), y_mesh.ravel()]))
+
+    cf = ax.contourf(x_range, y_range, 
+                     z_mesh.T.reshape(x_mesh.shape), cmap = cmap)
+    return cf
