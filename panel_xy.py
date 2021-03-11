f419348780f3e6414cc4438e507eedffb1f76802
ypwong22 <ypwong22@users.noreply.github.com>
2021-03-11 00:15:02 -0500

    Update panel_xy.py

--- panel_xy.py
+++ panel_xy.py
@@ -1,6 +1,35 @@
 import numpy as np
 
 ####
+from statsmodels.regression.linear_model import OLS
+from statsmodels.tools.tools import add_constant
+
+from tools.format_text import ppf
+
+def ax_regress(ax, vector, pos_xy = [0.1, 0.9],
+             args_pt = {'ls': '-'},
+             args_ln = {'color': 'k'},
+             args_tx = {'color': 'k'}):
+    """
+    Plot the time series with trend.
+    """
+    x = np.arange(len(vector))
+    temp = ~np.isnan(vector)
+    x = x[temp]
+    vector = vector[temp]
+
+    ax.plot(x, vector, **args_pt)
+
+    res = OLS(vector, add_constant(x)).fit()
+    ax.plot(x, x * res.params[1] + res.params[0], **args_ln)
+
+    ax.text(pos_xy[0], pos_xy[1],
+            ppf(res.params[1], res.params[0],
+                res.pvalues[1], res.pvalues[0]),
+            transform = ax.transAxes, **args_tx)
+
+
+####
 from scipy.stats import gaussian_kde
 
 def ax_scatter_density(ax, x, y, cmap = 'jet'):
@@ -11,7 +40,8 @@ def ax_scatter_density(ax, x, y, cmap = 'jet'):
     h = ax.scatter(x, y, c = density, cmap = cmap,
                    edgecolor = '')
     return h
-  
+
+
 ####
 from scipy.stats import gaussian_kde
 
