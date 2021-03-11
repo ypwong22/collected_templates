47d11027439bff9d1222021b3b47bf45e5e7b1a3
ypwong22 <ypwong22@users.noreply.github.com>
2021-03-11 12:53:02 -0500

    Update unit_funcs.py

--- tools/unit_funcs.py
+++ tools/unit_funcs.py
@@ -7,7 +7,7 @@ def _relative_entropy(vector, r_max):
     
     Parameters
     ----------
-    vector: ndarray
+    vector: 1-d array
         12-element vector corresponding to a 12-month climatology.
     r_max: float
         whether to normalize the annual total by the global maximum in order to
@@ -23,24 +23,60 @@ def _relative_entropy(vector, r_max):
     D = np.sum(frac_month * np.log2(frac_month * 12))
     return D
 
+
 def longterm_seasonality(vector, r_max):
     r"""
-    Uses relative entropy. Ranges between (0, log_2(12) = 3.585)
+    The long-term seasonality index defined in
+    
+    Feng, X., A. Porporato, and I. Rodriguez-Iturbe, 2013: Changes in rainfall seasonality
+    in the tropics. Nature Clim Change, 3, 811–815, https://doi.org/10.1038/nclimate1907.
+    
     Meausures the evenness of the distribution of precipitation over months.
-    Larger values means precipitation is more concentrated in the wet season.
+    Uses relative entropy and ranges between 0 and log_2(12) = 3.585, with larger
+    values meaning that precipitation is more concentrated in the wet season.
 
     Parameters
     ----------
-    vector: ndarray
+    vector: 1-d array
         12-element vector corresponding to a 12-month climatology.
     r_max: float
         whether to normalize the annual total by the global maximum in order to
         distinguish between different locations across the world
         (norm_frac = 7932 mm yr^{-1} at Tabubil, Papua New Guinea in the original paper).
-
-    Feng, X., A. Porporato, and I. Rodriguez-Iturbe, 2013: Changes in rainfall seasonality
-    in the tropics. Nature Clim Change, 3, 811–815, https://doi.org/10.1038/nclimate1907.
     """
     D = _relative_entropy(vector, r_max)
     S = D * np.sum(vector) / r_max
     return S
+
+
+def centroid(vector):
+    r"""
+    Measures the timing of rainfall using the first moment.
+
+    Feng, X., A. Porporato, and I. Rodriguez-Iturbe, 2013: Changes in rainfall seasonality
+    in the tropics. Nature Clim Change, 3, 811–815, https://doi.org/10.1038/nclimate1907.
+
+    Parameters
+    ----------
+    vector: 1-d array
+        12-element vector corresponding to a 12-month climatology.
+    """"    
+    C = np.sum( np.arange(1, 13) * vector ) / np.sum(vector)
+    return C
+
+def spread(vector, C = None):
+    r"""
+    Measures the duration of the wet season using the spread around the centroid.
+
+    Feng, X., A. Porporato, and I. Rodriguez-Iturbe, 2013: Changes in rainfall seasonality
+    in the tropics. Nature Clim Change, 3, 811–815, https://doi.org/10.1038/nclimate1907.
+
+    Parameters
+    ----------
+    vector: 1-d array
+        12-element vector corresponding to a 12-month climatology.
+    """
+    if C is None:
+        C = centroid(vector)
+    Z = np.sqrt( np.sum( np.power( np.arange(1, 13) - C, 2 ) * vector ) / np.sum(vector) )
+    return Z
