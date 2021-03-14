6a96304c65736bb9bd5e5cc4aabba5b718719665
ypwong22 <ypwong22@users.noreply.github.com>
2021-03-14 16:24:46 -0400

    Update panel_stats.py

--- panel_stats.py
+++ panel_stats.py
@@ -10,43 +10,51 @@ def ax_histogram(ax, vector, bins, dist = 'norm',
                  args_line = {'color': '#2b8cbe'}):
     """
     Plot a histogram in panel with fitted Gaussian distribution.
-    
+
     Parameters
     ----------
       vector: 1-d array
           The data set.
       bins: int or a sequence of increasing numbers
-          The same as input into **numpy.histogram**. 
+          The same as input into **numpy.histogram**.
           Defines the number of bins or the bin edges, from left-most to
           right-most.
       dist: str
-          If *norm*, plot fitted normal distribution. 
+          If *norm*, plot fitted normal distribution.
           If *kde*, plot fitted Gaussian kernal density.
       show: str
-          If *both* (default), plot both the histogram bars and fitted 
+          If *both* (default), plot both the histogram bars and fitted
           distribution.
           If *bar*, plot only the histogram bars.
           If *line*, plot only the fitted distribution using lines.
     """
     vector = vector[~np.isnan(vector)]
     hist, bin_edges = np.histogram(vector, bins = bins, density = True)
-    
+
+    h = []
+
     if (show == 'bar') | (show == 'both'):
-        ax.bar(bin_edges[:-1], hist, width = bin_edges[1:] - bin_edges[:-1],
-               align = 'edge', **args_hist)
-    
-    x = np.linspace(bin_edges[0], bin_edges[-1], 100)
+        h1 = ax.bar(bin_edges[:-1], hist,
+                    width = bin_edges[1:] - bin_edges[:-1],
+                    align = 'edge', **args_hist)
+        h.append(h1)
 
     if (show == 'line') | (show == 'both'):
+        x = np.linspace(bin_edges[0], bin_edges[-1], 100)
+
         if dist == 'norm':
             mean, std = norm.fit(vector)
-            ax.plot(x, norm.pdf(x, mean, std), **args_line)
+            h2, = ax.plot(x, norm.pdf(x, mean, std), **args_line)
         elif dist == 'kde':
             kde = KernelDensity(bandwidth = np.std(vector)/5,
                                 kernel='gaussian')
             kde.fit(vector)
             prob = np.exp(kde.score_samples(x))
-            ax.plot(x, prob, '-', **args_line)
+            h2, = ax.plot(x, prob, '-', **args_line)
+        h.append(h2)
+
+    return h
+
 
 ####
 from statsmodels.tsa.stattools import acf
