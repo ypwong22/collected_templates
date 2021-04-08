8f27e28eb912bd04bc99a778e8b434dc9d065d17
ypwong22 <ypwong22@users.noreply.github.com>
2021-04-08 19:28:25 -0400

    Update analysis_3d.py

--- analysis_3d.py
+++ analysis_3d.py
@@ -1,49 +1,5 @@
 ###############################################################################
-# Parallel calculation of trend for each grid of a 3D [time, lat, lon]
-# xr.DataAarray.
-###############################################################################
-import xarray as xr
-import numpy as np
-import multiprocessing as mp
-import statsmodels.api as stats
-
-data = xr.open_dataset('myfile.nc')
-var = data['var'].values.copy()
-data.close()
-
-var = np.reshape(var, [var.shape[0], var.shape[1] * var_shape[2]])
-
-retain = np.where(~np.isnan(var[0, :]))[0]
-var = np.array_split(var[:, retain], len(retain), axis = 1)
-
-def calc_trend(vector, rank):
-  vector = vector.reshape(-1)
-  mod = stats.OLS(vector, stats.add_constant(range(len(vector))))
-  reg = mod.fit()
-  intercept, slope = reg.params
-  p_slope = reg.pvalues[1]
-  slope_CI_lower, slope_CI_upper = reg.conf_int()[1, :]
-  return (slope, p_slope, slope_CI_lower, slope_CI_upper, rank)
-
-pool = mp.Pool(mp.cpu_count() - 1)
-results = [pool.apply_async(calc_trend, args = (var1, ind)) for ind, var1 in enumerate(var)]
-pool.close()
-pool.join()
-
-result = [r.get() for r in result]
-
-trend = np.full(len(data.lat.values) * len(data.lon.values))
-for n in range(len(results)):
-  trend[retain[results[n][-1]]] = results[n][0]
-trend = xr.DataArray(trend.reshape(len(data.lat.values),
-                                   len(data.lon.values)),
-                     coords = {'lat': data.lat.values, 
-                               'lon': data.lon.values},
-                     dims = ['lat', 'lon'])
-
-
-###############################################################################
-# Easier calculation of trend for each grid of a 3D [time, lat, lon]
+# Fast calculation of trend for each grid of a 3D [time, lat, lon]
 # xr.DataAarray.
 ###############################################################################
 import xarray as xr
@@ -105,3 +61,13 @@ mean = da.resample({'time': '1M'}).mean()
 target_lat = np.arange(-89.75, 89.76, 0.5)
 target_lon = np.arange(-179.75, 179.76, 0.5)
 da.interp(coords = {'lon': target_lon, 'lat': target_lat}).load()
+
+
+###############################################################################
+# Flip longitude from 360 to -180 to 180.
+###############################################################################
+def flip_lon(da):
+    lon_dim = [dd for dd in da.dims if dd.lower() in ['lon','longitude']][0]
+    da = da.roll(np.sum(da[lon_dim] > 180), roll_coords = lon_dim)
+    da[lon_dim][(da[lon_dim] > 180)] = 360. - da[lon_dim]
+    return da
