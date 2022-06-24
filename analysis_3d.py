6fc4356fedd98647136ac60bfd094d5c5e7b4c2a
ypwong22 <ypwong22@users.noreply.github.com>
2022-06-24 10:52:49 -0400

    Update analysis_3d.py

--- analysis_3d.py
+++ analysis_3d.py
@@ -20,6 +20,112 @@ def detrend(sst):
    sst = sst - trend
    return sst, trend
 
+###############################################################################
+# Fast calculation of trend for each grid of a 3D [time, lat, lon]
+# np.ma.array.
+###############################################################################
+def _olsTensor(Y, x):
+    """ Repeated calculation of linear regression in the spatial dimensions.
+    Parameters
+    ----------
+    Y : np.ma.array
+        The variable of interest. The first dimension will be assumed to be
+        time (replicate observations).
+    x : np.array or np.ma.array
+        The time variable of interest. If one-dimensional, will be propagated
+        to the dimensionality of Y. If having the same dimensionality as Y,
+        must be a masked array.
+    Returns
+    -------
+    r : np.ma.array
+        The trend. If x only has a time dimension, `r` is a scalar.
+        Otherwise, `r` has the same dimensionality as x[1:].
+    p : np.ma.array
+        The two-sided p-values of the trend. If x only has a time 
+        dimension, `p` is a scalar. Otherwise, `p` has the same 
+        dimensionality as x[1:].
+    """
+    if type(Y) != np.ma.core.MaskedArray:
+        raise TypeError('Y must be a masked array')
+    if Y.shape[0] < 3:
+        raise ValueError('At least three observations are needed')
+
+    if (type(x) != np.ma.core.MaskedArray) and (type(x) != np.ndarray):
+        raise TypeError('x must be either masked or ordinary numpy array')
+    if (not np.allclose(x.shape, Y.shape)) and (len(x.shape) != 1):
+        raise ValueError('x must be either 1-dimensional or has the same shape as Y')
+
+    # homogenize the shape and mask of x and Y
+    if type(Y.mask) == bool:
+        Y.mask = np.full(Y.shape, Y.mask)
+    if type(x) == np.ma.core.MaskedArray:
+        if type(x.mask) == bool:
+            x.mask = np.full(x.shape, x.mask)
+    else:
+        x = np.ma.array(x, mask = np.full(x.shape, False))
+
+    orig_shape = Y.shape
+    Y = Y.reshape(Y.shape[0], 1, int(np.prod(Y.shape[1:])))
+    if len(x.shape) != 1:
+        x = x.reshape(Y.shape)
+    else:
+        x = np.ma.array(np.broadcast_to(x.data.reshape(-1,1,1), Y.shape),
+                        mask = np.broadcast_to(x.mask.reshape(-1,1,1), Y.shape))
+    x = np.ma.array(x.data, mask = x.mask | Y.mask)
+    Y = np.ma.array(Y, mask = x.mask)
+
+    # normalize
+    x, _, x_scale = _normalize(x)
+    Y, _, Y_scale = _normalize(Y)
+
+    # add constant term
+    x = np.ma.concatenate([np.ma.array(np.ones(Y.shape), mask = Y.mask), x], axis = 1)
+
+    # calculate the regression coefficients; treating the masked points as if zero.
+    xx = np.where(x.mask == False, x.data, 0.)
+    yy = np.where(Y.mask == False, Y.data, 0.)
+    beta = np.einsum('ijk,jlk->ilk',
+                     np.einsum('ijk,ljk->ilk',
+                               np.linalg.pinv(np.einsum('ijk,ilk->jlk',xx,xx \
+                                                    ).transpose(2,0,1)).transpose(1,2,0),
+                               xx), yy)
+    # calculate the p-value
+    from scipy.stats import t
+    dof = np.sum(np.ma.getmaskarray(Y)== False, axis = 0) - 2
+    resid = yy - np.einsum('ijk,jlk->ilk', xx, beta)
+    mse = np.sum(np.power(resid,2), axis=0) / dof
+    # somehow, unable to apply np.ma.mean on x[:,[1],:]
+    temp = x[:,[1],:]
+    temp.data[temp.mask] = np.nan
+    temp = temp.data
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore", category = RuntimeWarning)
+        std = np.nansum(np.power(temp - np.nanmean(temp, axis = 0, keepdims = True), 2), axis = 0)
+
+    # somehow, using masked array here results in underflow error; had to use np.nan
+    np.seterr(divide='ignore', invalid='ignore')
+    beta = beta[1, :] # discard intercept
+    tval = beta / np.sqrt(mse/std)
+    np.seterr(divide='raise', invalid='raise')
+    pval = 2 * t.sf(np.abs(tval), dof)
+
+    # scale the beta
+    beta = beta * Y_scale / x_scale
+
+    # mask the data
+    tval = np.ma.masked_invalid(tval)
+    pval = np.ma.array(pval, mask = tval.mask)
+    beta = np.ma.array(beta, mask = tval.mask)
+
+    # restore shape
+    if len(orig_shape) > 1:
+        beta = beta.reshape(orig_shape[1:])
+        pval = pval.reshape(orig_shape[1:])
+    else:
+        beta = float(beta.data)
+        pval = float(pval.data)
+    return beta, pval
+
 
 ###############################################################################
 # Calculate the annual + seasonal average of a xarray DataArray. Assuming the
